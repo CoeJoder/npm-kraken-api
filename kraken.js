462e3cf69ab1541df49b92dc7cca6f8470f20ce5
CoeJoder <CoeJoder@users.noreply.github.com>
2017-12-12 05:42:41 -0800

    Changed to 3rd party lib for crypto hashing: jsSHA

--- kraken.js
+++ kraken.js
@@ -1,61 +1,136 @@
-const got    = require('got');
-const crypto = require('crypto');
-const qs     = require('qs');
+const got = require("got");
+const jsSHA = require("jssha");
 
 // Public/Private method names
 const methods = {
-	public  : [ 'Time', 'Assets', 'AssetPairs', 'Ticker', 'Depth', 'Trades', 'Spread', 'OHLC' ],
-	private : [ 'Balance', 'TradeBalance', 'OpenOrders', 'ClosedOrders', 'QueryOrders', 'TradesHistory', 'QueryTrades', 'OpenPositions', 'Ledgers', 'QueryLedgers', 'TradeVolume', 'AddOrder', 'CancelOrder', 'DepositMethods', 'DepositAddresses', 'DepositStatus', 'WithdrawInfo', 'Withdraw', 'WithdrawStatus', 'WithdrawCancel' ],
+	public: [
+		"Time",
+		"Assets",
+		"AssetPairs",
+		"Ticker",
+		"Depth",
+		"Trades",
+		"Spread",
+		"OHLC"
+	],
+	private: [
+		"Balance",
+		"TradeBalance",
+		"OpenOrders",
+		"ClosedOrders",
+		"QueryOrders",
+		"TradesHistory",
+		"QueryTrades",
+		"OpenPositions",
+		"Ledgers",
+		"QueryLedgers",
+		"TradeVolume",
+		"AddOrder",
+		"CancelOrder",
+		"DepositMethods",
+		"DepositAddresses",
+		"DepositStatus",
+		"WithdrawInfo",
+		"Withdraw",
+		"WithdrawStatus",
+		"WithdrawCancel"
+	]
 };
 
 // Default options
 const defaults = {
-	url     : 'https://api.kraken.com',
-	version : 0,
-	timeout : 5000,
+	url: "https://api.kraken.com",
+	version: 0,
+	timeout: 5000
 };
 
 // Create a signature for a request
 const getMessageSignature = (path, request, secret, nonce) => {
-	const message       = qs.stringify(request);
-	const secret_buffer = new Buffer(secret, 'base64');
-	const hash          = new crypto.createHash('sha256');
-	const hmac          = new crypto.createHmac('sha512', secret_buffer);
-	const hash_digest   = hash.update(nonce + message).digest('binary');
-	const hmac_digest   = hmac.update(path + hash_digest, 'binary').digest('base64');
+	const message = qs.stringify(request);
+	const secret_buffer = new Buffer(secret, "base64");
+	const hash = new crypto.createHash("sha256");
+	const hmac = new crypto.createHmac("sha512", secret_buffer);
+	const hash_digest = hash.update(nonce + message).digest("binary");
+	const hmac_digest = hmac
+		.update(path + hash_digest, "binary")
+		.digest("base64");
 
 	return hmac_digest;
+};
+
+const getKrakenSignature = async (path, postdata, nonce) => {
+	var sha256obj = new jsSHA("SHA-256", "BYTES");
+	sha256obj.update(nonce + postdata);
+	var hash_digest = sha256obj.getHash("BYTES");
+
+	var sha512obj = new jsSHA("SHA-512", "BYTES");
+	sha512obj.setHMACKey(api_key_private, "B64");
+	sha512obj.update(path);
+	sha512obj.update(hash_digest);
+	return sha512obj.getHMAC("B64");
 };
 
 // Send an API request
 const rawRequest = async (url, headers, data, timeout) => {
 	// Set custom User-Agent string
-	headers['User-Agent'] = 'Kraken Javascript API Client';
+	headers["User-Agent"] = "Kraken Javascript API Client";
 
 	const options = { headers, timeout };
 
 	Object.assign(options, {
-		method : 'POST',
-		body   : qs.stringify(data),
+		method: "POST",
+		body: qs.stringify(data)
 	});
 
 	const { body } = await got(url, options);
 	const response = JSON.parse(body);
 
-	if(response.error && response.error.length) {
+	if (response.error && response.error.length) {
 		const error = response.error
-			.filter((e) => e.startsWith('E'))
-			.map((e) => e.substr(1));
+			.filter(e => e.startsWith("E"))
+			.map(e => e.substr(1));
 
-		if(!error.length) {
+		if (!error.length) {
 			throw new Error("Kraken API returned an unknown error");
 		}
 
-		throw new Error(error.join(', '));
+		throw new Error(error.join(", "));
 	}
 
 	return response;
 };
+
+// Send an API request
+// const gappRequest = async (url, headers, data, timeout) => {
+// 	// Set custom User-Agent string
+// 	headers["User-Agent"] = "Kraken Javascript API Client";
+
+// 	const options = { headers, timeout };
+
+// 	Object.assign(options, {
+// 		method: "POST",
+// 		body: qs.stringify(data)
+// 	});
+
+// 	const { body } = await got(url, options);
+// 	const response = JSON.parse(body);
+
+// 	var response = UrlFetchApp.fetch (url, options);
+
+// 	if (response.error && response.error.length) {
+// 		const error = response.error
+// 			.filter(e => e.startsWith("E"))
+// 			.map(e => e.substr(1));
+
+// 		if (!error.length) {
+// 			throw new Error("Kraken API returned an unknown error");
+// 		}
+
+// 		throw new Error(error.join(", "));
+// 	}
+
+// 	return response;
+// };
 
 /**
  * KrakenClient connects to the Kraken.com API
@@ -68,8 +143,8 @@ const rawRequest = async (url, headers, data, timeout) => {
 class KrakenClient {
 	constructor(key, secret, options) {
 		// Allow passing the OTP as the third argument for backwards compatibility
-		if(typeof options === 'string') {
-			options = { otp : options };
+		if (typeof options === "string") {
+			options = { otp: options };
 		}
 
 		this.config = Object.assign({ key, secret }, defaults, options);
@@ -84,19 +159,17 @@ class KrakenClient {
 	 */
 	api(method, params, callback) {
 		// Default params to empty object
-		if(typeof params === 'function') {
+		if (typeof params === "function") {
 			callback = params;
-			params   = {};
+			params = {};
 		}
 
-		if(methods.public.includes(method)) {
+		if (methods.public.includes(method)) {
 			return this.publicMethod(method, params, callback);
-		}
-		else if(methods.private.includes(method)) {
+		} else if (methods.private.includes(method)) {
 			return this.privateMethod(method, params, callback);
-		}
-		else {
-			throw new Error(method + ' is not a valid API method.');
+		} else {
+			throw new Error(method + " is not a valid API method.");
 		}
 	}
 
@@ -111,19 +184,19 @@ class KrakenClient {
 		params = params || {};
 
 		// Default params to empty object
-		if(typeof params === 'function') {
+		if (typeof params === "function") {
 			callback = params;
-			params   = {};
+			params = {};
 		}
 
-		const path     = '/' + this.config.version + '/public/' + method;
-		const url      = this.config.url + path;
+		const path = "/" + this.config.version + "/public/" + method;
+		const url = this.config.url + path;
 		const response = rawRequest(url, {}, params, this.config.timeout);
 
-		if(typeof callback === 'function') {
+		if (typeof callback === "function") {
 			response
-				.then((result) => callback(null, result))
-				.catch((error) => callback(error, null));
+				.then(result => callback(null, result))
+				.catch(error => callback(error, null));
 		}
 
 		return response;
@@ -140,19 +213,19 @@ class KrakenClient {
 		params = params || {};
 
 		// Default params to empty object
-		if(typeof params === 'function') {
+		if (typeof params === "function") {
 			callback = params;
-			params   = {};
+			params = {};
 		}
 
-		const path = '/' + this.config.version + '/private/' + method;
-		const url  = this.config.url + path;
+		const path = "/" + this.config.version + "/private/" + method;
+		const url = this.config.url + path;
 
-		if(!params.nonce) {
+		if (!params.nonce) {
 			params.nonce = new Date() * 1000; // spoof microsecond
 		}
 
-		if(this.config.otp !== undefined) {
+		if (this.config.otp !== undefined) {
 			params.otp = this.config.otp;
 		}
 
@@ -164,16 +237,16 @@ class KrakenClient {
 		);
 
 		const headers = {
-			'API-Key'  : this.config.key,
-			'API-Sign' : signature,
+			"API-Key": this.config.key,
+			"API-Sign": signature
 		};
 
 		const response = rawRequest(url, headers, params, this.config.timeout);
 
-		if(typeof callback === 'function') {
+		if (typeof callback === "function") {
 			response
-				.then((result) => callback(null, result))
-				.catch((error) => callback(error, null));
+				.then(result => callback(null, result))
+				.catch(error => callback(error, null));
 		}
 
 		return response;
